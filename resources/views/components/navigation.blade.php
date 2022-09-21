1ad0f736c1f647452470f32a46d2e59a16bab239
ARRW-Core <raig.ramzy@gmail.com>
2022-09-21 09:13:06 +0500

    Init

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -70,32 +70,41 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-{{--    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">--}}
-{{--        <div class="pt-2 pb-3 space-y-1">--}}
-{{--            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-{{--                {{ __('Dashboard') }}--}}
-{{--            </x-responsive-nav-link>--}}
-{{--        </div>--}}
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
 
-{{--        <!-- Responsive Settings Options -->--}}
-{{--        <div class="pt-4 pb-1 border-t border-gray-200">--}}
-{{--            <div class="px-4">--}}
-{{--                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>--}}
-{{--                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>--}}
-{{--            </div>--}}
+        <!-- Responsive Settings Options -->
+        <div class="pt-4 pb-1 border-t border-gray-200">
+            @if (!Auth::user())
+                <div class="px-4">
+                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                </div>
+            @endif
+            <div class="mt-3 space-y-1">
+                <!-- Authentication -->
+                @if (!Auth::user())
+                    <x-responsive-nav-link :href="route('logout')"
+                                           onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-responsive-nav-link>
+                @else
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
 
-{{--            <div class="mt-3 space-y-1">--}}
-{{--                <!-- Authentication -->--}}
-{{--                <form method="POST" action="{{ route('logout') }}">--}}
-{{--                    @csrf--}}
-
-{{--                    <x-responsive-nav-link :href="route('logout')"--}}
-{{--                            onclick="event.preventDefault();--}}
-{{--                                        this.closest('form').submit();">--}}
-{{--                        {{ __('Log Out') }}--}}
-{{--                    </x-responsive-nav-link>--}}
-{{--                </form>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+                        <x-responsive-nav-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                @endif
+            </div>
+        </div>
+    </div>
 </nav>
