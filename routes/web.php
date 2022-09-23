c98173eb75f55388fcd277a5d426fd1404cb050e
ARRW-Core <raig.ramzy@gmail.com>
2022-09-23 20:32:53 +0500

    Added pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,10 +16,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/news', function () {
+    return view('pages.news');
+})->name('news');
+
+Route::get('/politics', function () {
+    return view('pages.politics');
+})->name('politics');
+
+Route::get('/world-news', function () {
+    return view('pages.world-news');
+})->name('world-news');
+
+Route::get('/business', function () {
+    return view('pages.business');
+})->name('business');
+
+Route::get('/sports', function () {
+    return view('pages.sports');
+})->name('sports');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/dashboard/news-overview', function () {
+    return view('pages.dashboard.news-overview');
+})->middleware(['auth'])->name('news-overview');
 
 require __DIR__.'/auth.php';
